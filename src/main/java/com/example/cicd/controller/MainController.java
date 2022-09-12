f74cb768f15b689b12ba2818ceebe0adfc157f72
Eric <ksh117@kakao.com>
2022-09-12 21:47:34 +0900

    docker file 등록, 컨트롤러 등록

---
+++ src/main/java/com/example/cicd/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.example.cicd.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Eric
+ * @Description
+ * @Since 22. 9. 12.
+ **/
+@RestController
+@RequestMapping("/api/v1/main")
+public class MainController {
+
+    @Value("${env.name}")
+    private String envName;
+
+    @GetMapping
+    public String main() {
+        return "Hello World! => " + envName;
+    }
+}
